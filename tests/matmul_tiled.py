3b3c72c604c73cd3e3aef3426051f2de452cc85d
Laz4rz <boronskimikolaj@gmail.com>
2024-10-13 23:41:44 +0000

    now GEMM is working + random tests passing

--- tests/matmul_tiled.py
+++ tests/matmul_tiled.py
@@ -28,6 +28,7 @@ void matmulKernel(float *A, float *B, float *C, int m, int n, int o) {
 
     float acc = 0.0;
     int globMemAcc = 0;
+    float temp;
 
     for (int tile=0; tile<(n + TILE_WIDTH - 1)/TILE_WIDTH; ++tile) {
 
@@ -35,19 +36,28 @@ void matmulKernel(float *A, float *B, float *C, int m, int n, int o) {
             Mds[ty][tx] = A[row * n + tx + tile * TILE_WIDTH];
             globMemAcc += 1;
         }
+        else {
+            Mds[ty][tx] = 0.0;
+        }
 
         if (column < o && (ty + tile * TILE_WIDTH ) < n) {
             Nds[ty][tx] =  B[column + ty * o + tile * TILE_WIDTH * o];
             globMemAcc += 1;
         }
+        else {
+            Nds[ty][tx] = 0.0;
+        }
         __syncthreads();                           
   
         for (int i=0; i<TILE_WIDTH; ++i) {
-            acc += Mds[ty][i] * Nds[i][tx];
+            temp = Mds[ty][i] * Nds[i][tx];
+            // if (column == 0 && row == 0) 
+                // printf("Mds[%d][%d] * Nds[%d][%d] = %f * %f = %d\\n", ty, i, i, tx, Mds[ty][i], Nds[i][tx], temp);
+            acc += temp;
         }
         __syncthreads();
     }
-    printf("Thread (%d, %d) accessed %d global memory locations\\n", row, column, globMemAcc);
+    // printf("Thread (%d, %d) accessed %d global memory locations\\n", row, column, globMemAcc);
     if (row < m && column < o) {
         C[row * o + column] = acc;
     }
@@ -59,9 +69,9 @@ module = SourceModule(cuda_code)
 matmul_kernel = module.get_function("matmulKernel")
 
 # Allocate and initialize arrays on the host
-N = 7
-M = 2
-O = 7
+N = 5
+M = 7
+O = 9
 a = np.arange(N*M).astype(np.float32)
 b = np.arange(M*O).astype(np.float32)
 c = np.zeros(N*O).astype(np.float32)
@@ -102,6 +112,35 @@ cuda.memcpy_dtoh(c, c_gpu)
 
 gt = np.dot(a.reshape(N, M), b.reshape(M, O)).reshape(-1)
 
-assert np.allclose(c, gt), f"Error: {c} != {gt}"
-print("Result:")
+passed = np.allclose(c, gt)
+assert passed, f"Error: {c} != {gt}"
+print(f"Result ({passed}):")
 print_row_major(c, N, O)
+
+# tests
+if True:
+    for i in range(50):
+        M = np.random.randint(1, 100)
+        N = np.random.randint(1, 100)
+        O = np.random.randint(1, 100)
+        a = np.random.rand(N * M).astype(np.float32)
+        b = np.random.rand(M * O).astype(np.float32)
+        c = np.zeros(N*O).astype(np.float32)
+
+        a_gpu = cuda.mem_alloc(a.nbytes)
+        b_gpu = cuda.mem_alloc(b.nbytes)
+        c_gpu = cuda.mem_alloc(c.nbytes)
+
+        cuda.memcpy_htod(a_gpu, a)
+        cuda.memcpy_htod(b_gpu, b)
+
+        block_size = (TILE_WIDTH, TILE_WIDTH, 1)
+        grid_size = ((O + TILE_WIDTH - 1) // TILE_WIDTH, (N + TILE_WIDTH - 1) // TILE_WIDTH, 1)
+
+        matmul_kernel(a_gpu, b_gpu, c_gpu, np.int32(N), np.int32(M), np.int32(O), block=block_size, grid=grid_size)
+        cuda.Context.synchronize()
+
+        cuda.memcpy_dtoh(c, c_gpu)
+
+        gt = np.dot(a.reshape(N, M), b.reshape(M, O)).reshape(-1)
+        print(f"({M}, {N}, {O}) Passed: {np.allclose(c, gt)}")
